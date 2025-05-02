db17d96d35b73e8eee8fa526ae307a457ad62272
AHGhaderi <62669364+AHGhaderi@users.noreply.github.com>
2025-05-02 10:12:21 -0600

    Entropy of graph

---
+++ G_Entropy.m
@@ -0,0 +1,10 @@
+function S=G_Entropy(A)
+% entropy of a graph 
+% A is a symmetric adjacency matrix
+% developted by Amirhossein Ghaderi
+% Cite: Ghaderi, A. H., Baltaretu, B. R., Andevari, M. N., Bharmauria, V., & Balci, F. (2020). 
+% Synchrony and complexity in state-related EEG networks: an application of spectral graph theory. Neural Computation, 32(12), 2422-2454.
+
+S=entropy(A);
+
+end
